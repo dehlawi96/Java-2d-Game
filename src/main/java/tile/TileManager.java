c3f761a5c2257b339d3cf55f3360dd3b122e18ea
Soheb <muhammadsoheb91@gmail.com>
2025-12-17 18:44:34 +0000

    Background Tiles

--- src/main/java/tile/TileManager.java
+++ src/main/java/tile/TileManager.java
@@ -2,6 +2,9 @@ package tile;
 
 import com.LoL.GamePanel;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 
 public class TileManager {
@@ -17,5 +20,40 @@ public class TileManager {
 
     public void getTileImage() {
 
+        try {
+
+            tiles[0] = new Tile();
+            tiles[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/grass.png"));
+
+            tiles[1] = new Tile();
+            tiles[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/wall.png"));
+
+            tiles[2] = new Tile();
+            tiles[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/water.png"));
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void draw(Graphics g2d) {
+
+        int col = 0;
+        int row = 0;
+        int x = 0;
+        int y = 0;
+
+        while (col < gp.maxScreenCol && row < gp.maxScreenRow) {
+
+            g2d.drawImage(tiles[0].image, x, y, gp.tileSize, gp.tileSize, null);
+            col++;
+            x += gp.tileSize;
+
+            if (col == gp.maxScreenCol) {
+                col = 0;
+                x = 0;
+                row++;
+                y += gp.tileSize;
+            }
+        }
     }
 }
